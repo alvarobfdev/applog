8a9c4cf11bd097f64006d8c1efce59082f2b78ad
Álvaro Baño Fos <alvarobanofos@gmail.com>
2016-02-19 13:17:44 +0100

    API facturas

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,11 +10,20 @@ namespace App\Http\Controllers;
 
 
 
+use App\Models\FacturasWeb;
+use App\User;
 use Carbon\Carbon;
 
 class ApiController extends Controller
 {
-    public function postPdfInvoice() {
+
+    private $webInvoicesToAdd = array();
+    private $webInvoicesToModify = array();
+    private $webInvoicesToDelete = array();
+
+
+    public function postPdfInvoice()
+    {
         $data = \Request::get("jsonData");
         $data = utf8_encode($data);
         $facturas = json_decode($data, true);
@@ -25,10 +34,10 @@ class ApiController extends Controller
         /*Generamos ZIP*/
         $uid = uniqid();
         $zip = new \Chumper\Zipper\Zipper;
-        $zip->make(storage_path() ."/app/tmp/$uid/facturas.zip");
+        $zip->make(storage_path() . "/app/tmp/$uid/facturas.zip");
 
         /*Insertamos cada factura en el zip*/
-        foreach($facturas as $factura) {
+        foreach ($facturas as $factura) {
             $view = $this->viewInvoice($factura);
 
             $nombreFact = "factura-{$factura['id']['serfac']}-{$factura['id']['ejefac']}-{$factura['id']['numfac']}.pdf";
@@ -38,16 +47,17 @@ class ApiController extends Controller
 
         /*Cerramos ZIP, eliminamos temportal y  descargamos*/
         $zip->close();
-        $response = \Response::make(file_get_contents(storage_path() ."/app/tmp/$uid/facturas.zip"));
+        $response = \Response::make(file_get_contents(storage_path() . "/app/tmp/$uid/facturas.zip"));
         \Storage::drive("local")->deleteDirectory("tmp/$uid");
         $response->header('Content-Disposition', 'attachment; filename="facturas.zip"');
-        $response->header('Content-Length',strlen($response->getOriginalContent()));
+        $response->header('Content-Length', strlen($response->getOriginalContent()));
 
         return $response;
     }
 
 
-    public function postPdfInvoicesSummary() {
+    public function postPdfInvoicesSummary()
+    {
         $data = \Request::get("jsonData");
         $facturas = json_decode($data, true);
         $data['facturas'] = $facturas;
@@ -57,7 +67,8 @@ class ApiController extends Controller
 
     }
 
-    private function groupsInvoice($factura) {
+    private function groupsInvoice($factura)
+    {
         $groups = array();
 
         foreach ($factura['lines'] as $index => $line) {
@@ -73,7 +84,8 @@ class ApiController extends Controller
         return $groups;
     }
 
-    private function viewInvoice($factura) {
+    private function viewInvoice($factura)
+    {
 
         $data['factura'] = $factura;
         $data['groups'] = $this->groupsInvoice($factura);
@@ -81,80 +93,196 @@ class ApiController extends Controller
         return $view;
     }
 
-    public function postRefreshPdfWeb() {
+    public function postRefreshPdfWeb()
+    {
 
         try {
             $data = \Request::get("jsonData");
             $data = utf8_encode($data);
             $facturas = json_decode($data, true);
             $uidFolder = uniqid();
-            \File::makeDirectory(storage_path("app") . "/tmp/$uidFolder");
+            $tmpDir = "/tmp/$uidFolder";
+            \File::makeDirectory(storage_path("app") . $tmpDir);
 
             $ftpConnection = \FTP::connection();
             $facturasPost = array();
 
+            $return = "ok";
 
             foreach ($facturas as $factura) {
 
 
-                $view = $this->viewInvoice($factura);
-                $view = str_replace("localhost:8080", "localhost", $view);
+                if ($factura["web"]) {
 
-                $uidPdf = uniqid();
-
-                $pdfPath = storage_path("app") . "/tmp/$uidFolder/$uidPdf.pdf";
-
-                \PDF::loadHTML($view)->setPaper('a4')->setOption('margin-right', 0)
-                    ->setOption('margin-bottom', 0)->setOption('margin-left', 0)->setOption('margin-top', 0)
-                    ->save($pdfPath);
-
-                if(!$ftpConnection->uploadFile($pdfPath, "httpsdocs/facturas/$uidPdf.pdf")) {
-                    return "error: Fallo al cargar factura $uidPdf.pdf a la web";
+                    if ($this->webInvoiceExists($factura)) {
+                        $return = $this->addWebInvoiceToModify($factura, $tmpDir);
+                    } else {
+                        $return = $this->addWebInvoiceToAdd($factura, $tmpDir);
+                    }
+                } else {
+                    if ($this->webInvoiceExists($factura)) {
+                        $return = $this->addWebInvoiceToDelete($factura);
+                    }
                 }
 
-
-                \File::deleteDirectory($pdfPath);
-
-
-
-                $facturasPost[] = [
-                    "fecha" => Carbon::parse($factura["fecfac"])->format("Y-m-d"),
-                    "ejercicio" => $factura["id"]["ejefac"],
-                    "num_factura" => $factura["id"]["numfac"],
-                    "cliente_id" => $factura["codcli"],
-                    "total_factura" => $factura["totfac"],
-                    "file_pdf" => "$uidPdf.pdf"
-                ];
-
+                if ($return != "ok") {
+                    return "error: " . $return;
+                }
             }
 
-            $postData = ["jsonData" => json_encode($facturasPost)];
-            $result = $this->sendPostRequest("https://clientes.logival.es/api.php?user=api&pass=logivalapp&function=addInvoice", $postData);
+            \File::deleteDirectory($tmpDir);
 
-            if($result != "ok") {
-                return "error: ".$result;
+
+            if(count($this->webInvoicesToAdd)> 0) {
+                $postData = ["jsonData" => json_encode($facturasPost)];
+                $return = $this->sendPostRequest("https://clientes.logival.es/api.php?user=api&pass=logivalapp&function=addInvoice", $postData);
+            }
+
+            if(count($this->webInvoicesToModify)> 0) {
+                $postData = ["jsonData" => json_encode($facturasPost)];
+                $return = $this->sendPostRequest("https://clientes.logival.es/api.php?user=api&pass=logivalapp&function=modifyInvoice", $postData);
+            }
+
+            if(count($this->webInvoicesToDelete)> 0) {
+                $postData = ["jsonData" => json_encode($facturasPost)];
+                $return = $this->sendPostRequest("https://clientes.logival.es/api.php?user=api&pass=logivalapp&function=deleteInvoice", $postData);
+            }
+
+
+            if ($return != "ok") {
+                return "error: " . $return;
             }
 
             return "ok";
 
 
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return "error: " . $e->getMessage();
         }
     }
 
-    private function sendPostRequest($url, $data) {
+    private function sendPostRequest($url, $data)
+    {
         // use key 'http' even if you send the request to https://...
         $options = array(
             'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
                 'content' => http_build_query($data),
             ),
         );
-        $context  = stream_context_create($options);
+        $context = stream_context_create($options);
         $result = file_get_contents($url, false, $context);
         return $result;
+    }
+
+    private function webInvoiceExists($factura) {
+
+        return (FacturasWeb::where("ejercicio", $factura["ejercicio"])->where("num_factura", $factura["num_factura"])->count() > 0);
+    }
+
+    private function addWebInvoiceToAdd($factura, $tmpDir)
+    {
+        $ftpConnection = \FTP::connection();
+
+        list($uidPdf, $pdfPath) = $this->savePdf($factura, $tmpDir);
+
+        if (!$ftpConnection->uploadFile($pdfPath, "httpsdocs/facturas/$uidPdf.pdf")) {
+            return "error: Fallo al cargar factura $uidPdf.pdf a la web";
+        }
+
+        //Guardamos en bbdd intermedia
+        $facturaModel = new FacturasWeb();
+        $facturaModel->fecha = Carbon::parse($factura["fecfac"])->format("Y-m-d");
+        $facturaModel->ejercicio = $factura["id"]["ejefac"];
+        $facturaModel->num_factura = $factura["id"]["numfac"];
+        $facturaModel->cliente_id = $factura["codcli"];
+        $facturaModel->total_factura = $factura["totfac"];
+        $facturaModel->file_pdf = "$uidPdf.pdf";
+        $facturaModel->save();
+
+        $this->webInvoicesToAdd[] = [
+            "fecha" => $facturaModel->fecha,
+            "ejercicio" => $facturaModel->ejercicio,
+            "num_factura" => $facturaModel->num_factura,
+            "cliente_id" => $facturaModel->cliente_id,
+            "total_factura" => $facturaModel->total_factura,
+            "file_pdf" => $facturaModel->file_pdf
+        ];
+
+        \FTP::disconnect();
+
+        return "ok";
+    }
+
+    private function addWebInvoiceToModify($factura, $tmpDir) {
+        $facturaModel = FacturasWeb::where("ejercicio", $factura["ejercicio"])->where("num_factura", $factura["num_factura"])->firstOrFail();
+        $pdfName = $facturaModel->file_pdf;
+
+        $ftpConnection = \FTP::connection();
+        $ftpConnection->delete("httpsdocs/facturas/$pdfName");
+
+        list($uidPdf, $pdfPath) = $this->savePdf($factura, $tmpDir);
+
+        if (!$ftpConnection->uploadFile($pdfPath, "httpsdocs/facturas/$uidPdf.pdf")) {
+            return "error: Fallo al cargar factura $uidPdf.pdf a la web";
+        }
+
+        $facturaModel->fecha = Carbon::parse($factura["fecfac"])->format("Y-m-d");
+        $facturaModel->cliente_id = $factura["codcli"];
+        $facturaModel->total_factura = $factura["totfac"];
+        $facturaModel->file_pdf = "$uidPdf.pdf";
+        $facturaModel->save();
+
+        $this->webInvoicesToModify[] = [
+            "fecha" => $facturaModel->fecha,
+            "ejercicio" => $facturaModel->ejercicio,
+            "num_factura" => $facturaModel->num_factura,
+            "cliente_id" => $facturaModel->cliente_id,
+            "total_factura" => $facturaModel->total_factura,
+            "file_pdf" => $facturaModel->file_pdf
+        ];
+
+        \FTP::disconnect();
+
+        return "ok";
+    }
+
+    private function addWebInvoiceToDelete($factura) {
+        $facturaModel = FacturasWeb::where("ejercicio", $factura["ejercicio"])->where("num_factura", $factura["num_factura"])->firstOrFail();
+        $pdfName = $facturaModel->file_pdf;
+        $ftpConnection = \FTP::connection();
+        $ftpConnection->delete("httpsdocs/facturas/$pdfName");
+
+        $this->webInvoicesToDelete[] = [
+            "fecha" => $facturaModel->fecha,
+            "ejercicio" => $facturaModel->ejercicio,
+            "num_factura" => $facturaModel->num_factura,
+            "cliente_id" => $facturaModel->cliente_id,
+            "total_factura" => $facturaModel->total_factura,
+            "file_pdf" => $facturaModel->file_pdf
+        ];
+
+        $facturaModel->delete();
+        return "ok";
+    }
+
+    /**
+     * @param $factura
+     * @param $tmpDir
+     * @return array
+     */
+    private function savePdf($factura, $tmpDir)
+    {
+        $view = $this->viewInvoice($factura);
+        $view = str_replace("localhost:8080", "localhost", $view);
+        $uidPdf = uniqid();
+        $pdfPath = storage_path("app") . "$tmpDir/$uidPdf.pdf";
+
+        \PDF::loadHTML($view)->setPaper('a4')->setOption('margin-right', 0)
+            ->setOption('margin-bottom', 0)->setOption('margin-left', 0)->setOption('margin-top', 0)
+            ->save($pdfPath);
+        return array($uidPdf, $pdfPath);
     }
 
 
---
+++ app/Models/FacturasWeb.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alvarobanofos
+ * Date: 19/2/16
+ * Time: 11:34
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class FacturasWeb extends Model
+{
+    protected $table = "facturas_web";
+}
